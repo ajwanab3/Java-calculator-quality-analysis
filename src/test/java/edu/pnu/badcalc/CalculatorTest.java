d0f14c18398e922bb04ebffc94ee64edef4e6c4a
Ajwan Alomar <ajwanab3@gmail.com>
2025-11-13 19:17:12 +0300

    Trigger SonarCloud analysis nalysis

--- src/test/java/edu/pnu/badcalc/CalculatorTest.java
+++ src/test/java/edu/pnu/badcalc/CalculatorTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     // ------- Tests for add() -------
+// update trigger
 
     @Test
     public void testAddTwoPositiveNumbers() {
